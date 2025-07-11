aff9815f63e84a91516e4141e4495c45650d544d
Matrix030 <rishigharat@gmail.com>
2025-07-11 08:53:18 -0400

    updated system_utils to get system resources for dynamic allocation

--- utils/system_utils.py
+++ utils/system_utils.py
@@ -1,5 +1,25 @@
 import psutil
 
+def get_system_resources():
+    total_memory =  psutil.virtual_memory().total / (1024 ** 3)
+    cpu_count = psutil.cpu_count(logical=False)
+    if not cpu_count:
+        cpu_count = psutil.cpu_count(logical=True) 
+
+    dask_memory = int(total_memory * 1)
+    worker_count =  max(1, cpu_count)
+    memory_per_worker = int(dask_memory / worker_count)
+
+    return {
+        'worker_count': worker_count,
+        'memory_per_worker': memory_per_worker,
+        'total_memory': total_memory
+    }
+
+def estimated_file_size(file):
+    return file.size / (1024 ** 3)
+
+
 def format_time(seconds):
     if seconds is None:
         return "Not Complete"
